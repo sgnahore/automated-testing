e2eba98ed6a22ce316958a3698e6dfe22490f57c
Sgnahore <stephaniergnahore@gmail.com>
2023-09-28 14:15:20 +0100

    back up

--- src/find-longest-string.test.js
+++ src/find-longest-string.test.js
@@ -4,8 +4,12 @@ const findLongestString = require("./find-longest-string");
 test("findLongestString finds the longest string in an array", () => {
   expect(findLongestString(["it", "is", "a", "nice", "day"])).toBe("nice");
   expect(findLongestString(["why", "hello", "to", "you"])).toBe("hello");
+  expect(findLongestString(["active", "trial", "to", "win", "lose"])).toBe(
+    "active"
+  );
 });
 
 test("findLongestString returns the earlier string in cases of joint longest strings", () => {
   expect(findLongestString(["brave", "dance"])).toBe("brave");
+  expect(findLongestString(["dan", "jess", "kate"])).toBe("jess");
 });
